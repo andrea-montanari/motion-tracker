61adc075561eb272c3b283af85af1a5a67a4fca9
Andrea <montanari.andrea98@gmail.com>
2023-12-27 09:58:55 +0100

    FEAT: add max variance and std deviation calculation
    Also applied small fix

--- lib/Utils/RunningStat.dart
+++ lib/Utils/RunningStat.dart
@@ -2,14 +2,16 @@ import 'dart:math';
 
 class RunningStat {
   late int _n;
-  late double _oldM, _newM, _oldS, _newS;
+  late double _oldM, _newM, _oldS, _newS, _var, _std, _maxVar, _maxStd;
 
   RunningStat() {
     _n = 0;
+    _maxVar = _maxStd = 0.0;
   }
 
   void clear() {
     _n = 0;
+    _maxVar = _maxStd = 0.0;
   }
 
   void push(double x) {
@@ -20,11 +22,19 @@ class RunningStat {
       _oldS = 0.0;
     } else {
       _newM = _oldM + (x - _oldM) / _n;
-      _newS = _oldS + (x - _oldM) / (x - _newM);
+      _newS = _oldS + (x - _oldM) * (x - _newM);
 
       // Set up for next iteration
       _oldM = _newM;
       _oldS = _newS;
+
+      // Calculate variance and std
+      _var = ((_n > 1) ? _newS / (_n - 1) : 0.0);
+      _std = sqrt(_var);
+
+      // Calculate maximum variance and std
+      _maxVar = _var > _maxVar ? _var : _maxVar;
+      _maxStd = _std > _maxStd ? _std : _maxStd;
     }
   }
 
@@ -37,10 +47,18 @@ class RunningStat {
   }
 
   double variance() {
-    return ((_n > 1) ? _newS / (_n - 1) : 0.0);
+    return _var;
   }
 
   double standardDeviation() {
-    return sqrt(variance());
+    return _std;
+  }
+
+  double maxVariance() {
+    return _maxVar;
+  }
+
+  double maxStd() {
+    return _maxStd;
   }
 }
